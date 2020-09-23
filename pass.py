a2185f8ce9aeac7b0ec7af04d87f2e577ab511e2
Alan Kao <alanods3on@gmail.com>
2020-09-23 21:49:23 +0800

    Fianl commit

--- pass.py
+++ pass.py
@@ -3,13 +3,15 @@
 #迴圈變數就請改變不然密碼並不會變
 #或改變while True --> while n > 0: (則最後一段if就無需要)
 n = 3
-while True:
+while n > 0:
+	n = n-1
 	password = input('請輸入密碼： ')
 	if password == 'a123456':
 		print('登入成功')
 		break
 	else:
-		n = n-1
-		print('密碼錯誤','還有',n,'次機會')
-		if n == 0:
-			break
+		print('密碼錯誤')
+		if n > 0:
+			print('還有',n,'次機會')
+		else:
+			print('沒機會嘗試了,帳號已鎖定')	
